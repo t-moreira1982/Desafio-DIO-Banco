78b32c8f773cbdcf6506503cb3462cb3b36ac70e
t-moreira1982 <thiago_moreira22@hotmail.com>
2022-08-02 18:53:03 +0044

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+			Cliente thiago = new Cliente();
+			thiago.setNome("Thiago");
+			
+			Conta cc = new ContaCorrente(thiago);
+			Conta poupanca = new ContaPoupanca(thiago);
+
+			cc.depositar(100);
+			cc.transferir(100, poupanca);
+			
+			cc.imprimirExtrato();
+			poupanca.imprimirExtrato();
+		}
+
+
+
+}
+
+
